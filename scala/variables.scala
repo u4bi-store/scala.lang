b4781362175c1e6f779b12e14af523c648b95f14
u4bi <u4bi@u4bi.com>
2016-12-25 04:10:16 +0900

    variables primitive types

--- scala/variables.scala
+++ scala/variables.scala
@@ -1,6 +1,12 @@
-object Variables
+object Variables{
+
   var x =5
   val y = "u4bi" /* const */
+  
+  /* primitive types : string boolean int double long char unit null*/
+  val a = 5 / 3
+  val b = 5.0 / 3
+  val c = 5 / 3.0
 
-  def main(args: Array[String]) = println(x+" - "+y)
+  def main(args: Array[String]) = println(x+" - "+y+" a-"+a+" b-"+b+" c-"+c)
 }
