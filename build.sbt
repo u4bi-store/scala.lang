7b830848e6e7613f59cb23294d986d0fcc7e7b86
u4bi <u4bi@u4bi.com>
2016-12-25 12:45:38 +0900

    jquery init

--- build.sbt
+++ build.sbt
@@ -3,3 +3,6 @@ enablePlugins(ScalaJSPlugin)
 name := "Scala.js Tutorial"
 
 scalaVersion := "2.12.0"
+
+libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.9.1"
+
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,9 +1,11 @@
 package tutorial.webapp
 
 import scala.scalajs.js.JSApp
+import org.scalajs.jquery.jQuery
 
 object TutorialApp extends JSApp {
   def main(): Unit = {
     println("u4bi")
+    jQuery("body").append("<p>[message]</p>")
   }
 }
