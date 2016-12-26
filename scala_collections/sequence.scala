841a887695029fc374393e49ccfaf80395403be1
u4bi <u4bi@u4bi.com>
2016-12-27 00:32:27 +0900

    sequence head, tail, last, init

--- scala_collections/sequence.scala
+++ scala_collections/sequence.scala
@@ -9,5 +9,18 @@ object Sequence{
     println("forall : "+seq.forall(x => x > 0) ) /* js array every */
     println("exists : "+seq.exists(x => x % 3 == 0) ) /* js array some */
     println("find : "+seq.find(x => x >4) ) /* js array find */
+
+    println("head : "+seq.head)
+    println("tail : "+seq.tail)
+    println("last : "+seq.last)
+    println("init : "+seq.init)
+//    println("drop : "+seq.drop(2))
+//    println("dropRight : "+seq.dropRight(2))
+//    println("count : "+seq.count(x => x < 3) )
+//    println("groupBy : "+seq.groupBy(x => x % 2) )
+//    println("sortBy : "+seq.sortBy(x=> x > 3) )
+//    println("partition : "+seq.partition(x=> x > 3) )
+//    println("seq:+6"+seq:+6)
+//    println("seq++"+seq++)
   }
 }
