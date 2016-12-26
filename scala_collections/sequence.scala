68c4292128d142f25bce47678c33511418e7b941
u4bi <u4bi@u4bi.com>
2016-12-27 00:25:39 +0900

    sequence contains, forall, exists, find

--- scala_collections/sequence.scala
+++ scala_collections/sequence.scala
@@ -5,5 +5,9 @@ object Sequence{
    
     println(seq)
     println("Empty : "+seq.isEmpty)
+    println("contains : "+seq.contains(6)) /* js array indexOf */
+    println("forall : "+seq.forall(x => x > 0) ) /* js array every */
+    println("exists : "+seq.exists(x => x % 3 == 0) ) /* js array some */
+    println("find : "+seq.find(x => x >4) ) /* js array find */
   }
 }
