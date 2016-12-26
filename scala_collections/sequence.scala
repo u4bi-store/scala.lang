fb7f144a4d5719e4a22e344a027732189e65a670
u4bi <u4bi@u4bi.com>
2016-12-27 00:40:24 +0900

    sequence drop

--- scala_collections/sequence.scala
+++ scala_collections/sequence.scala
@@ -10,11 +10,11 @@ object Sequence{
     println("exists : "+seq.exists(x => x % 3 == 0) ) /* js array some */
     println("find : "+seq.find(x => x >4) ) /* js array find */
 
-    println("head : "+seq.head)
-    println("tail : "+seq.tail)
-    println("last : "+seq.last)
-    println("init : "+seq.init)
-//    println("drop : "+seq.drop(2))
+//    println("head : "+seq.head)
+//    println("tail : "+seq.tail)
+//    println("last : "+seq.last)
+//    println("init : "+seq.init)
+    println("drop : "+seq.drop(2))
 //    println("dropRight : "+seq.dropRight(2))
 //    println("count : "+seq.count(x => x < 3) )
 //    println("groupBy : "+seq.groupBy(x => x % 2) )
