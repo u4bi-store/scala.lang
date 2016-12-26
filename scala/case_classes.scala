7c7b4aafb6931c8b731b49c508635a63c4628c56
u4bi <u4bi@u4bi.com>
2016-12-26 18:42:54 +0900

    case classes render

--- scala/case_classes.scala
+++ scala/case_classes.scala
@@ -5,8 +5,16 @@ object Case_Classes{
   
   case class Person(first:String, last:String)
   val person = Person("James","Bond")
+  
+
+  case class AX(a: Int, x: String)
+
+  val c = AX(1, "test")
+  val d = c.copy(a = 42)
 
   def main(args: Array[String]): Unit = {
     println(person)
+    println(c)
+    println(d)
   }
 }
